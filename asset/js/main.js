8eefdf058499b7018530d656dca6d8c35332d6dd
gaianacheli <gaianacheli04@gmail.com>
2021-10-29 14:46:27 +0200

    stampati a schermo oggetti array

---
+++ asset/js/main.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+       counter:0,
+        slides : [
+            {
+                title: 'svezia',
+                image: 'img/01.jpg',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                title: 'Svizzera',
+                image: 'img/02.jpg',
+                text:  'Lorem ipsum',
+            },
+            {
+                title: 'Gran Bretagna',
+                image: 'img/03.jpg',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                title: 'Germania',
+                image: 'img/04.jpg',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                title: 'Paradise',
+                image: 'img/05.jpg',
+                text:  'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ],
+        
+    }
+
+})
+
+
+
+
+
